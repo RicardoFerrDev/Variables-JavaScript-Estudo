c58d84d35c8f2a55c0d16d08a7dc903fe326ea0c
RicardoFerrDev <ricardo.bubel@gmail.com>
2022-09-22 13:06:59 -0300

    TiposVariáveis3

---
+++ TipoVariável.js
@@ -0,0 +1,10 @@
+ //Atividade 2 
+ //Indique o tipo de variável dos dados abaixo:
+
+//nome: String (Uma String nada mais é que texto puro.)
+//sobrenome: String (Uma String nada mais é que texto puro.)
+//peso: Number (São os números, seja eles integer, float, double etc.)
+//altura: Number (São os números, seja eles integer, float, double etc.)
+//dataDeInternacao: Number (São os números, seja eles integer, float, double etc.)
+//diabético: Booleans (Verdadeiro ou falso )
+
---
+++ programaSaude.js
@@ -0,0 +1,13 @@
+let peso, nome, altura, datainternacao, diabetes
+
+ nome = prompt("Informe seu nome: ");   
+ peso = prompt("Informe seu peso: ");
+ altura = prompt("Informe sua altura: ");
+ diabetes = prompt("Você é diabético? ");
+ datainternacao = prompt("Informe a data de internação: ");
+
+ alert("Nome: " + nome
+    + "\nPeso: " + peso
+    + "\nAltura: " + altura
+    + "\nData de internação: " + datainternacao
+    + "\nPaciente diabético : ." + diabetes)
